6fdada9776e16371931173edfe75e8b513c6b16c
Ty Gooch <gooch.ty@gmail.com>
2018-02-06 19:13:03 -0800

    moved zoom control to bottom right

--- src/frontend/components/CampusMap/CampusMap.js
+++ src/frontend/components/CampusMap/CampusMap.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup, Polygon } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup, Polygon, ZoomControl } from 'react-leaflet'
 import Locations from '../Location/locations'
 
 class CampusMap extends React.Component {
   render() {
     const position = [34.4127, -119.8492]
     return (
-      <Map id='map' center={position} zoom={16}>
+      <Map id='map' center={position} zoom={16} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
           url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png'
@@ -14,6 +14,7 @@ class CampusMap extends React.Component {
         
         <Locations />
         
+        <ZoomControl position="bottomright" />
       </Map>
     );
   }
